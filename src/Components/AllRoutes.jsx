be39ebe69f91e5ed1499af6e227671e88e8bd7b7
awasthi2001 <akshanshawasthi013@gmail.com>
2023-06-17 08:48:14 +0530

    added NotFound Route

--- src/Components/AllRoutes.jsx
+++ src/Components/AllRoutes.jsx
@@ -1,6 +1,7 @@
 import {Routes,Route} from 'react-router-dom'
 import { GitHubIssuesPage } from '../Pages/GitHubIssuesPage'
 import { SingleIssue } from '../Pages/SingleIssue'
+import NotFound from './NotFound'
 
 export function AllRoutes(){
 
@@ -9,6 +10,7 @@ export function AllRoutes(){
         <Routes>
           <Route path='/' element={<GitHubIssuesPage/>}></Route>
           <Route path='/issue/:id' element={<SingleIssue/>}></Route>
+          <Route path='*' element={<NotFound/>} />
         </Routes>
     )
 }
